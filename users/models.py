bae6e4920252a6c609894ee1c02dcaf5c51e8b50
Alexey <bystrovalexey15@mail.ru>
2025-05-20 01:56:52 +0700

    hw 6-27 + verification-email

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     country = models.CharField(max_length=30, blank=True, null=True)
+    token = models.CharField(max_length=100, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username',]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,16 @@
 import os
+import secrets
 
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from django.core.mail import send_mail
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
 from dotenv import load_dotenv
 
 from .forms import CustomUserCreationForm
-
+from .models import CustomUser
 
 load_dotenv(override=True)
 
@@ -21,13 +22,22 @@ class RegisterView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
-        login(self.request, user)
-        self.send_welcome_email(user.email)
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/email-confirm/{token}'
+        send_mail(
+            subject='Добро пожаловать в интернет-магазин BystrovStore!',
+            message = f'Спасибо, что зарегистрировались в нашем сервисе! Для подтверждения почты перейди по ссылке {url}',
+            from_email=os.getenv('EMAIL_HOST_USER'),
+            recipient_list=[user.email]
+        )
         return super().form_valid(form)
 
-    def send_welcome_email(self, user_email):
-        subject = 'Добро пожаловать в интернет-магазин BystrovStore!'
-        message = 'Спасибо, что зарегистрировались в нашем сервисе!'
-        from_email = os.getenv('EMAIL_HOST_USER')
-        recipient_list = [user_email]
-        send_mail(subject, message, from_email, recipient_list)
+def email_verification(request, token):
+    user = get_object_or_404(CustomUser, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse('users:login'))
